4fd12e1fe391a58beeefdd04349757b876d7d306
Taylor Huff <dancing_floie@yahoo.com>
2016-10-19 13:44:41 +0000

    Project 1

---
+++ js/main.js
@@ -0,0 +1,571 @@
+function checkTest() {
+
+var userScore = 0;
+
+    var answer1 = document.querySelector('input[name="q1"]:checked').value;
+
+    var answer2 = document.querySelector('input[name="q2"]:checked').value;
+
+    var answer3 = document.querySelector('input[name="q3"]:checked').value;
+
+    var answer4 = document.querySelector('input[name="q4"]:checked').value;
+
+    var answer5 = document.querySelector('input[name="q5"]:checked').value;
+
+    var answer6 = document.querySelector('input[name="q6"]:checked').value;
+
+    var answer7 = document.querySelector('input[name="q7"]:checked').value;
+
+    var answer8 = document.querySelector('input[name="q8"]:checked').value;
+
+    var answer9 = document.querySelector('input[name="q9"]:checked').value;
+
+    var answer10 = document.querySelector('input[name="q10"]:checked').value;
+
+    var answer11 = document.querySelector('input[name="q11"]:checked').value;
+
+    var answer12 = document.querySelector('input[name="q12"]:checked').value;
+
+    var answer13 = document.querySelector('input[name="q13"]:checked').value;
+
+    var answer14 = document.querySelector('input[name="q14"]:checked').value;
+
+    var answer15 = document.querySelector('input[name="q15"]:checked').value;
+
+    var answer16 = document.querySelector('input[name="q16"]:checked').value;
+
+    var answer17 = document.querySelector('input[name="q17"]:checked').value;
+
+    var answer18 = document.querySelector('input[name="q18"]:checked').value;
+
+    var answer19 = document.querySelector('input[name="q19"]:checked').value;
+
+    var answer20 = document.querySelector('input[name="q20"]:checked').value;
+
+// QUESTION 1
+    console.log("You chose " + userScore);
+    
+    if (answer1 === "A") {
+        userScore += 10;
+    } else if (answer1 === "B") {
+        userScore += 20;
+    } else if (answer1 === "C") {
+        userScore += 30;
+    } else if (answer1 === "D") {
+        userScore += 40;
+    } else if (answer1 === "E") {
+        userScore += 50;
+    } else if (answer1 === "F") {
+        userScore += 60;
+    } else if (answer1 === "G") {
+        userScore += 70;
+    } else if (answer1 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 2
+console.log("Your score is " + userScore);
+
+    if (answer2 === "A") {
+        userScore += 10;
+    } else if (answer2 === "B") {
+        userScore += 20;
+    } else if (answer2 === "C") {
+        userScore += 30;
+    } else if (answer2 === "D") {
+        userScore += 40;
+    } else if (answer2 === "E") {
+        userScore += 50;
+    } else if (answer2 === "F") {
+        userScore += 60;
+    } else if (answer2 === "G") {
+        userScore += 70;
+    } else if (answer2 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 3
+console.log("Your score is " + userScore);
+
+    if (answer3 === "A") {
+        userScore += 10;
+    } else if (answer3 === "B") {
+        userScore += 20;
+    } else if (answer3 === "C") {
+        userScore += 30;
+    } else if (answer3 === "D") {
+        userScore += 40;
+    } else if (answer3 === "E") {
+        userScore += 50;
+    } else if (answer3 === "F") {
+        userScore += 60;
+    } else if (answer3 === "G") {
+        userScore += 70;
+    } else if (answer3 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 4
+console.log("Your score is " + userScore);
+
+    if (answer4 === "A") {
+        userScore += 10;
+    } else if (answer4 === "B") {
+        userScore += 20;
+    } else if (answer4 === "C") {
+        userScore += 30;
+    } else if (answer4 === "D") {
+        userScore += 40;
+    } else if (answer4 === "E") {
+        userScore += 50;
+    } else if (answer4 === "F") {
+        userScore += 60;
+    } else if (answer4 === "G") {
+        userScore += 70;
+    } else if (answer4 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 5
+console.log("Your score is " + userScore);
+
+    if (answer5 === "A") {
+        userScore += 10;
+    } else if (answer5 === "B") {
+        userScore += 20;
+    } else if (answer5 === "C") {
+        userScore += 30;
+    } else if (answer5 === "D") {
+        userScore += 40;
+    } else if (answer5 === "E") {
+        userScore += 50;
+    } else if (answer5 === "F") {
+        userScore += 60;
+    } else if (answer5 === "G") {
+        userScore += 70;
+    } else if (answer5 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 6
+console.log("Your score is " + userScore);
+
+    if (answer6 === "A") {
+        userScore += 10;
+    } else if (answer6 === "B") {
+        userScore += 20;
+    } else if (answer6 === "C") {
+        userScore += 30;
+    } else if (answer6 === "D") {
+        userScore += 40;
+    } else if (answer6 === "E") {
+        userScore += 50;
+    } else if (answer6 === "F") {
+        userScore += 60;
+    } else if (answer6 === "G") {
+        userScore += 70;
+    } else if (answer6 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 7
+console.log("Your score is " + userScore);
+
+    if (answer7 === "A") {
+        userScore += 10;
+    } else if (answer7 === "B") {
+        userScore += 20;
+    } else if (answer7 === "C") {
+        userScore += 30;
+    } else if (answer7 === "D") {
+        userScore += 40;
+    } else if (answer7 === "E") {
+        userScore += 50;
+    } else if (answer7 === "F") {
+        userScore += 60;
+    } else if (answer7 === "G") {
+        userScore += 70;
+    } else if (answer7 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 8
+console.log("Your score is " + userScore);
+
+    if (answer8 === "A") {
+        userScore += 10;
+    } else if (answer8 === "B") {
+        userScore += 20;
+    } else if (answer8 === "C") {
+        userScore += 30;
+    } else if (answer8 === "D") {
+        userScore += 40;
+    } else if (answer8 === "E") {
+        userScore += 50;
+    } else if (answer8 === "F") {
+        userScore += 60;
+    } else if (answer8 === "G") {
+        userScore += 70;
+    } else if (answer8 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 9
+console.log("Your score is " + userScore);
+
+    if (answer9 === "A") {
+        userScore += 10;
+    } else if (answer9 === "B") {
+        userScore += 20;
+    } else if (answer9 === "C") {
+        userScore += 30;
+    } else if (answer9 === "D") {
+        userScore += 40;
+    } else if (answer9 === "E") {
+        userScore += 50;
+    } else if (answer9 === "F") {
+        userScore += 60;
+    } else if (answer9 === "G") {
+        userScore += 70;
+    } else if (answer9 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 10
+console.log("Your score is " + userScore);
+
+    if (answer10 === "A") {
+        userScore += 10;
+    } else if (answer10 === "B") {
+        userScore += 20;
+    } else if (answer10 === "C") {
+        userScore += 30;
+    } else if (answer10 === "D") {
+        userScore += 40;
+    } else if (answer10 === "E") {
+        userScore += 50;
+    } else if (answer10 === "F") {
+        userScore += 60;
+    } else if (answer10 === "G") {
+        userScore += 70;
+    } else if (answer10 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 11
+console.log("Your score is " + userScore);
+
+    if (answer11 === "A") {
+        userScore += 10;
+    } else if (answer11 === "B") {
+        userScore += 20;
+    } else if (answer11 === "C") {
+        userScore += 30;
+    } else if (answer11 === "D") {
+        userScore += 40;
+    } else if (answer11 === "E") {
+        userScore += 50;
+    } else if (answer11 === "F") {
+        userScore += 60;
+    } else if (answer11 === "G") {
+        userScore += 70;
+    } else if (answer11 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 12
+console.log("Your score is " + userScore);
+
+    if (answer12 === "A") {
+        userScore += 10;
+    } else if (answer12 === "B") {
+        userScore += 20;
+    } else if (answer12 === "C") {
+        userScore += 30;
+    } else if (answer12 === "D") {
+        userScore += 40;
+    } else if (answer12 === "E") {
+        userScore += 50;
+    } else if (answer12 === "F") {
+        userScore += 60;
+    } else if (answer12 === "G") {
+        userScore += 70;
+    } else if (answer12 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 13
+console.log("Your score is " + userScore);
+
+    if (answer13 === "A") {
+        userScore += 10;
+    } else if (answer13 === "B") {
+        userScore += 20;
+    } else if (answer13 === "C") {
+        userScore += 30;
+    } else if (answer13 === "D") {
+        userScore += 40;
+    } else if (answer13 === "E") {
+        userScore += 50;
+    } else if (answer13 === "F") {
+        userScore += 60;
+    } else if (answer13 === "G") {
+        userScore += 70;
+    } else if (answer13 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 14
+console.log("Your score is " + userScore);
+
+    if (answer14 === "A") {
+        userScore += 10;
+    } else if (answer14 === "B") {
+        userScore += 20;
+    } else if (answer14 === "C") {
+        userScore += 30;
+    } else if (answer14 === "D") {
+        userScore += 40;
+    } else if (answer14 === "E") {
+        userScore += 50;
+    } else if (answer14 === "F") {
+        userScore += 60;
+    } else if (answer14 === "G") {
+        userScore += 70;
+    } else if (answer14 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 15
+console.log("Your score is " + userScore);
+
+    if (answer15 === "A") {
+        userScore += 10;
+    } else if (answer15 === "B") {
+        userScore += 20;
+    } else if (answer15 === "C") {
+        userScore += 30;
+    } else if (answer15 === "D") {
+        userScore += 40;
+    } else if (answer15 === "E") {
+        userScore += 50;
+    } else if (answer15 === "F") {
+        userScore += 60;
+    } else if (answer15 === "G") {
+        userScore += 70;
+    } else if (answer15 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 16
+console.log("Your score is " + userScore);
+
+    if (answer16 === "A") {
+        userScore += 10;
+    } else if (answer16 === "B") {
+        userScore += 20;
+    } else if (answer16 === "C") {
+        userScore += 30;
+    } else if (answer16 === "D") {
+        userScore += 40;
+    } else if (answer16 === "E") {
+        userScore += 50;
+    } else if (answer16 === "F") {
+        userScore += 60;
+    } else if (answer16 === "G") {
+        userScore += 70;
+    } else if (answer16 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 17
+console.log("Your score is " + userScore);
+
+    if (answer17 === "A") {
+        userScore += 10;
+    } else if (answer17 === "B") {
+        userScore += 20;
+    } else if (answer17 === "C") {
+        userScore += 30;
+    } else if (answer17 === "D") {
+        userScore += 40;
+    } else if (answer17 === "E") {
+        userScore += 50;
+    } else if (answer17 === "F") {
+        userScore += 60;
+    } else if (answer17 === "G") {
+        userScore += 70;
+    } else if (answer17 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 18
+console.log("Your score is " + userScore);
+
+    if (answer18 === "A") {
+        userScore += 10;
+    } else if (answer18 === "B") {
+        userScore += 20;
+    } else if (answer18 === "C") {
+        userScore += 30;
+    } else if (answer18 === "D") {
+        userScore += 40;
+    } else if (answer18 === "E") {
+        userScore += 50;
+    } else if (answer18 === "F") {
+        userScore += 60;
+    } else if (answer18 === "G") {
+        userScore += 70;
+    } else if (answer18 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 19
+console.log("Your score is " + userScore);
+
+    if (answer19 === "A") {
+        userScore += 10;
+    } else if (answer19 === "B") {
+        userScore += 20;
+    } else if (answer19 === "C") {
+        userScore += 30;
+    } else if (answer19 === "D") {
+        userScore += 40;
+    } else if (answer19 === "E") {
+        userScore += 50;
+    } else if (answer19 === "F") {
+        userScore += 60;
+    } else if (answer19 === "G") {
+        userScore += 70;
+    } else if (answer19 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// QUESTION 20
+console.log("Your score is " + userScore);
+
+    if (answer20 === "A") {
+        userScore += 10;
+    } else if (answer20 === "B") {
+        userScore += 20;
+    } else if (answer20 === "C") {
+        userScore += 30;
+    } else if (answer20 === "D") {
+        userScore += 40;
+    } else if (answer20 === "E") {
+        userScore += 50;
+    } else if (answer20 === "F") {
+        userScore += 60;
+    } else if (answer20 === "G") {
+        userScore += 70;
+    } else if (answer20 === "H") {
+        userScore += 80;
+    } else {
+        console.log("Gawd, pick one.");
+    }
+
+// USER SCORE
+console.log("Your score is " + userScore);
+
+    if (userScore <= 370) {
+        console.log("Office Chair");
+    } else if (userScore <= 570) {
+        console.log("Lazy Boy");
+    } else if (userScore <= 770) {
+        console.log("Stool");
+    } else if (userScore <= 970) {
+        console.log("Regular Chair");
+    } else if (userScore <= 1130) {
+        console.log("Contemporary Chair");
+    } else if (userScore <= 1270) {
+        console.log("Chaise Lounge");
+    } else if (userScore <= 1570) {
+        console.log("Love Seat");
+    } else if (userScore <= 1700) {
+        console.log("Rocking Chair");
+    } else {
+        console.log("You really screwed up, buddy.");
+    }
+
+    if (userScore <= 370) {
+        document.getElementById("userResult"
+            ).innerHTML="<a href='#'><img src='images/officechair.png' border=0/></a>";
+    	}
+
+    if (userScore > 370 && userScore <= 570) {
+        document.getElementById("userResult"
+            ).innerHTML = "<a href='#'><img src='images/lazyboy.png' border=0/></a>";
+    	}
+
+    if (userScore > 570 && userScore <= 770) {
+        document.getElementById("userResult"
+            ).innerHTML = "<a href='#'><img src='images/stool.png' border=0/></a>";
+  		}
+
+    if (userScore > 770 && userScore <= 970) {
+        document.getElementById("userResult"
+            ).innerHTML = "<a href='#'><img src='images/regularchair.png' border=0/></a>";
+        }
+
+    if (userScore > 970 && userScore <= 1130) {
+        document.getElementById("userResult"
+            ).innerHTML = "<a href='#'><img src='images/contemporarychair.png' border=0/></a>";
+        }
+
+    if (userScore > 1130 && userScore <= 1270) {
+        document.getElementById("userResult"
+            ).innerHTML = "<a href='#'><img src='images/chaiselounge.png' border=0/></a>";
+        }
+
+    if (userScore > 1270 && userScore <= 1570) {
+        document.getElementById("userResult"
+            ).innerHTML = "<a href='#'><img src='images/loveseat.png' border=0/></a>";
+        }
+
+    if (userScore > 1570 && userScore <= 1700) {
+        document.getElementById("userResult"
+            ).innerHTML = "<a href='#'><img src='images/rockingchair.png' border=0/></a>";
+        }
+
+    else {
+        console.log("SOUL CHAIR");
+    }
+}
